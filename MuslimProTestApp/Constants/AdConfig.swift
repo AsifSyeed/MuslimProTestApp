2147ee18ebe97c9b36e92f4d7bbe1efce3223cfc
AsifSyeed <asifsyeed666@gmail.com>
2024-12-04 15:32:10 +0600

    Add AdConfig

---
+++ MuslimProTestApp/Constants/AdConfig.swift
@@ -0,0 +1,15 @@
+//
+//  AdConfig.swift
+//  MuslimProTestApp
+//
+//  Created by Asif Reddot on 12/3/24.
+//
+
+import Foundation
+
+struct AdConfig {
+    static let interstitialAdUnitID = "ca-app-pub-3940256099942544/4411468910"
+    static let minimumAdInterval: TimeInterval = 120
+    static let appOpenAdUnitID = "ca-app-pub-3940256099942544/5575463023"
+    static let adExpirationTime: TimeInterval = 4 * 60 * 60
+}
